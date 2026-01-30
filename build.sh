240e7822a4ef47ba8ae4f9b45361cd8b0f910bac
MartoNievas <tinnievas@gmail.com>
2026-01-30 14:03:55 -0300

    Drag&Drop files implement

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
+
 set -xe
 
 CFLAGS="-Wall -Wextra -ggdb $(pkg-config --cflags raylib)"
 LIBS="$(pkg-config --libs raylib) -lm -ldl -lpthread"
 
-mkdir -p build
-
-if [ -n "$RELOAD" ]; then
-  # 🔥 plugin dinámico
-  gcc $CFLAGS -fPIC -shared src/plug.c -o build/libplug.so $LIBS
-  gcc $CFLAGS -DRELOAD src/music.c -o build/music $LIBS
+mkdir -p ./build/
+if [ ! -z "${HOTRELOAD}" ]; then
+  clang $CFLAGS -o ./build/libplug.so -fPIC -shared ./src/plug.c $LIBS
+  clang $CFLAGS -DHOTRELOAD -o ./build/music ./src/music.c $LIBS -L./build/
 else
-  # 🔒 modo estático
-  gcc $CFLAGS src/music.c src/plug.c -o build/music $LIBS
+  clang $CFLAGS -o ./build/music ./src/plug.c ./src/music.c $LIBS -L./build/
 fi
+clang -o ./build/fft ./src/fft.c -lm
--- src/plug.c
+++ src/plug.c
@@ -1,40 +1,42 @@
 #include "plug.h"
 #include <assert.h>
 #include <math.h>
-#include <raylib.h>
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define N 256
-static const float pi;
+
+#define N (1 << 13)
+#define FONT_SIZE 69
+
+typedef struct {
+  Music music;
+  Font font;
+  bool error;
+} Plug;
+
+Plug *plug = NULL;
 
 float in[N];
 float complex out[N];
-float max_amp;
 
-typedef struct {
-  short left;
-  short right;
-} Frame;
+// Ported from https://rosettacode.org/wiki/Fast_Fourier_transform#Python
+void fft(float in[], size_t stride, float complex out[], size_t n) {
+  assert(n > 0);
 
-void fft(float in[], int stride, float complex out[], size_t size) {
-  assert(size > 0);
-
-  if (size == 1) {
+  if (n == 1) {
     out[0] = in[0];
     return;
   }
 
-  fft(in, stride * 2, out, size / 2);
-  fft(in + stride, stride * 2, out + size / 2, size / 2);
+  fft(in, stride * 2, out, n / 2);
+  fft(in + stride, stride * 2, out + n / 2, n / 2);
 
-  for (size_t k = 0; k < size / 2; k++) {
-    float t = (float)k / size;
-    float complex v = cexpf(-I * 2.0f * pi * t) * out[k + size / 2];
+  for (size_t k = 0; k < n / 2; ++k) {
+    float t = (float)k / n;
+    float complex v = cexp(-2 * I * PI * t) * out[k + n / 2];
     float complex e = out[k];
-
     out[k] = e + v;
-    out[k + size / 2] = e - v;
+    out[k + n / 2] = e - v;
   }
 }
 
@@ -47,38 +49,40 @@ float amp(float complex z) {
 }
 
 void callback(void *bufferData, unsigned int frames) {
-  Frame *fs = bufferData;
+  // https://cdecl.org/?q=float+%28*fs%29%5B2%5D
+  float (*fs)[plug->music.stream.channels] = bufferData;
 
   for (size_t i = 0; i < frames; ++i) {
     memmove(in, in + 1, (N - 1) * sizeof(in[0]));
-    in[N - 1] = fs[i].left;
+    in[N - 1] = fs[i][0];
   }
 }
 
-void plug_hello(void) { printf("Hello from plugin\n"); }
+void plug_init(void) {
+  plug = malloc(sizeof(*plug));
+  assert(plug != NULL && "Buy more RAM lol");
+  memset(plug, 0, sizeof(*plug));
 
-void plug_init(Plug *plug, const char *file_path) {
-
-  if (!plug || !file_path) {
-    fprintf(stderr, "ERROR: Null pointer in plug_init");
-    return;
+  plug->font = LoadFontEx("./fonts/Alegreya-Regular.ttf", FONT_SIZE, NULL, 0);
+}
+Plug *plug_pre_reload(void) {
+  if (IsMusicStreamPlaying(plug->music)) {
+    DetachAudioStreamProcessor(plug->music.stream, callback);
   }
-
-  plug->music = LoadMusicStream(file_path);
-  printf("music.frameCount = %u\n", plug->music.frameCount);
-  printf("music.stream.sampleRate = %u\n", plug->music.stream.sampleRate);
-  printf("music.stream.sampleSize = %u\n", plug->music.stream.sampleSize);
-  printf("music.stream.channels = %u\n", plug->music.stream.channels);
-  // assert(plug->music.stream.sampleSize == 16);
-  // assert(plug->music.stream.channels == 2);
-
-  SetMusicVolume(plug->music, 0.5f);
-  PlayMusicStream(plug->music);
-  AttachAudioStreamProcessor(plug->music.stream, callback);
+  return plug;
 }
 
-void plug_update(Plug *plug) {
-  UpdateMusicStream(plug->music);
+void plug_post_reload(Plug *prev) {
+  plug = prev;
+  if (IsMusicStreamPlaying(plug->music)) {
+    AttachAudioStreamProcessor(plug->music.stream, callback);
+  }
+}
+
+void plug_update(void) {
+  if (IsMusicStreamPlaying(plug->music)) {
+    UpdateMusicStream(plug->music);
+  }
 
   if (IsKeyPressed(KEY_SPACE)) {
     if (IsMusicStreamPlaying(plug->music)) {
@@ -89,8 +93,38 @@ void plug_update(Plug *plug) {
   }
 
   if (IsKeyPressed(KEY_Q)) {
-    StopMusicStream(plug->music);
-    PlayMusicStream(plug->music);
+    if (IsMusicStreamPlaying(plug->music)) {
+      StopMusicStream(plug->music);
+      PlayMusicStream(plug->music);
+    }
+  }
+
+  if (IsFileDropped()) {
+    FilePathList droppedFiles = LoadDroppedFiles();
+    if (droppedFiles.count > 0) {
+      const char *file_path = droppedFiles.paths[0];
+
+      if (IsMusicStreamPlaying(plug->music)) {
+        StopMusicStream(plug->music);
+        UnloadMusicStream(plug->music);
+      }
+
+      plug->music = LoadMusicStream(file_path);
+
+      if (plug->music.stream.buffer != NULL) {
+        plug->error = false;
+        printf("music.frameCount = %u\n", plug->music.frameCount);
+        printf("music.stream.sampleRate = %u\n", plug->music.stream.sampleRate);
+        printf("music.stream.sampleSize = %u\n", plug->music.stream.sampleSize);
+        printf("music.stream.channels = %u\n", plug->music.stream.channels);
+        SetMusicVolume(plug->music, 0.5f);
+        AttachAudioStreamProcessor(plug->music.stream, callback);
+        PlayMusicStream(plug->music);
+      } else {
+        plug->error = true;
+      }
+    }
+    UnloadDroppedFiles(droppedFiles);
   }
 
   int w = GetRenderWidth();
@@ -99,45 +133,53 @@ void plug_update(Plug *plug) {
   BeginDrawing();
   ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xFF});
 
-  fft(in, 1, out, N);
+  if (IsMusicStreamPlaying(plug->music)) {
+    fft(in, 1, out, N);
 
-  float max_amp = 0.0f;
-  for (size_t i = 0; i < N; ++i) {
-    float a = amp(out[i]);
-    if (max_amp < a)
-      max_amp = a;
-  }
-
-  float step = 1.06;
-  size_t m = 0;
-  for (float f = 20.0f; (size_t)f < N; f *= step) {
-    m += 1;
-  }
-
-  float cell_width = (float)w / m;
-  m = 0;
-  for (float f = 20.0f; (size_t)f < N; f *= step) {
-    float f1 = f * step;
-    float a = 0.0f;
-    for (size_t q = (size_t)f; q < N && q < (size_t)f1; ++q) {
-      a += amp(out[q]);
+    float max_amp = 0.0f;
+    for (size_t i = 0; i < N; ++i) {
+      float a = amp(out[i]);
+      if (max_amp < a)
+        max_amp = a;
     }
-    a /= (size_t)f1 - (size_t)f + 1;
-    float t = a / max_amp;
-    DrawRectangle(m * cell_width, h / 2 - h / 2 * t, cell_width, h / 2 * t,
-                  GREEN);
-    // DrawCircle(m*cell_width, h/2, h/2*t, GREEN);
-    m += 1;
+
+    float step = 1.06;
+    size_t m = 0;
+    for (float f = 20.0f; (size_t)f < N; f *= step) {
+      m += 1;
+    }
+
+    float cell_width = (float)w / m;
+    m = 0;
+    for (float f = 20.0f; (size_t)f < N; f *= step) {
+      float f1 = f * step;
+      float a = 0.0f;
+      for (size_t q = (size_t)f; q < N && q < (size_t)f1; ++q) {
+        a += amp(out[q]);
+      }
+      a /= (size_t)f1 - (size_t)f + 1;
+      float t = a / max_amp;
+      DrawRectangle(m * cell_width, (float)h / 2 - (float)h / 2 * t, cell_width,
+                    (float)h / 2 * t, GREEN);
+      // DrawCircle(m*cell_width, h/2, h/2*t, GREEN);
+      m += 1;
+    }
+  } else {
+    const char *label;
+    Color color;
+    if (plug->error) {
+      label = "Could not load file";
+      color = RED;
+    } else {
+      label = "Drag&Drop Music Here";
+      color = WHITE;
+    }
+    Vector2 size = MeasureTextEx(plug->font, label, plug->font.baseSize, 0);
+    Vector2 position = {
+        (float)w / 2 - size.x / 2,
+        (float)h / 2 - size.y / 2,
+    };
+    DrawTextEx(plug->font, label, position, plug->font.baseSize, 0, color);
   }
   EndDrawing();
-}
-
-void plug_world(void) { printf("Foo Bar\n"); }
-
-void plug_pre_reload(Plug *plug) {
-  DetachAudioStreamProcessor(plug->music.stream, callback);
-}
-
-void plug_post_reload(Plug *plug) {
-  AttachAudioStreamProcessor(plug->music.stream, callback);
 }
